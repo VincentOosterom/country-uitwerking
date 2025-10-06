be2e6ae8d71aee480c3dc38fba168259314c37f7
Vincen Oosterom <vincent@oosteromstudio.nlm>
2025-10-06 17:25:13 +0200

    opdracht 2 uitgwerkt

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,16 @@
 import './App.css';
-import axios from "axios";
-import {useState} from "react";
+import Opdracht1 from "./Opdracht1.jsx";
+import Opdracht2 from "./Opdracht 2.jsx";
 
 
 function App() {
-    const [countries, setCountries] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [showButton, setShowButton] = useState(true);
-
-    async function HandleClick() {
-        try {
-            setLoading(true);
-            const result = await axios.get('https://restcountries.com/v3.1/all?fields=name,flags,population');
-            setCountries(result.data);
-            setShowButton(false);
-        } catch (e) {
-            console.error("Fout bij het ophalen van alle landen", e);
-        } finally {
-            setLoading(false);
-        }
-    }
 
 
     return (
-        <>
-            <header>
-                <img src="src/assets/world_map.png" alt="World Map"/>
-                <h1>World Regions</h1>
-            </header>
-            <main className="main">
-                {showButton && <button onClick={HandleClick}>Toon alle landen</button>}
-                {loading && <p>Bezig met laden</p>}
-                <ul className="countries-list">
-                    {countries
-                        .sort((a, b) => a.population - b.population)
-                        .map((country, index) => (
-                        <li key={index}>
-                            <div className="country-name">
-                                <img
-                                    src={country.flags.svg}
-                                    alt={`Vlag van ${country.name.common}`}
-
-                                />
-                                <h3>{country.name.common}</h3>
-                            </div>
-                            <p>Has a population of {country.population.toLocaleString("nl-NL")}</p>
-                        </li>
-                    ))}
-                </ul>
-            </main>
-
-        </>
+        <div>
+            <Opdracht2/>
+        </div>
     )
 }
 
-export default App
+export default App;
---
+++ src/Opdracht 2.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import {useState} from "react";
+import './Opdracht2.css'
+
+function Opdracht2() {
+    const [query, setQuery] = useState("");
+    const [country, setCountry] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    // HELPERSFUNCTIE inwoners afronden tot miljoen
+
+    function formatPopulation(population) {
+        return Math.round(population / 100000)
+    }
+
+    async function SearchCountry(e) {
+        e.preventDefault();
+        if (!query) return
+
+        try {
+            setLoading(true);
+            setError("")
+            const response = await axios.get(`https://restcountries.com/v3.1/name/${query}?fullText=true`
+            );
+            console.log(response);
+            setCountry(response.data[0])
+            setQuery("")
+        } catch (e) {
+            console.log(e)
+            setError("Fout bij het laden van deze informatie");
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <header className="app-header">
+            <h1>Search Country Information</h1>
+            <img src="src/assets/world_map.png" alt=""/>
+            <form onSubmit={SearchCountry} className="search-country-form">
+                <input
+                    placeholder="Welke land zoekt u? "
+                    type="text"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button type="submit">Zoek het land</button>
+            </form>
+
+            {loading && <p>Bezig met ophalen van de juiste gegevens</p>}
+            {error && <p>{error}</p>}
+
+            {country && (
+                <article className="country-article">
+                    <div className="country-info">
+                        <img src={country.flags.svg} alt={`${country.name.common}`}/>
+                        <h3>{country.name.common}</h3>
+                    </div>
+                    <p>{country.name.common} is situated in {country.subregion} and the capital is {country.capital}. It
+                        has a population of {formatPopulation(country.population)} million people and it borders
+                        with {country.borders ? country.borders.length : 0} neighboring countries.</p>
+                </article>
+            )}
+        </header>
+    )
+}
+
+export default Opdracht2;
